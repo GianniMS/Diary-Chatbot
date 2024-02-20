ecf83f0c9ace8b1d43ceda0cd9b46baf2f9b175c
GianniMS <1008999@hr.nl>
2024-02-20 21:19:36 +0100

    small adjustment port console.log

--- server/server.js
+++ server/server.js
@@ -96,6 +96,6 @@ app.post('/chat', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Journal App listening on port ${port}`);
 });
 
